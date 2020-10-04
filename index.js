c557340db38862b578dcbf291cfd49f4f70f460d
Hayden Matz <haydenmatz97@gmail.com>
2020-10-04 23:08:14 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor((Math.random() * 100) + 1);
+  var product = { itemName: `${item}`, itemPrice: `${price}`}
+  cart.push(product)
+
+ return `${item} has been added to your cart.`// write your code here
 }
 
 function viewCart() {
-  // write your code here
+  var solution = [ "In your cart, you have " ]
+  if (cart.length === 0) {return "Your shopping cart is empty."}
+
+  for (var i=0; i < cart.length; i++){
+    var price = Math.floor((Math.random() * 100) + 1);
+    solution.push(`+ ${i} at, ${price} `)
+  }
+return solution  // write your code here
 }
 
 function total() {
