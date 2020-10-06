e7cdf09fff2af5dee09a8424a062c4bdc5f95abf
Hayden Matz <haydenmatz97@gmail.com>
2020-10-06 20:16:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var cart = [];
-
+var price = Math.floor((Math.random() * 100) + 1);
 function getCart() {
  return cart;
 }
@@ -10,23 +10,26 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  var price = Math.floor((Math.random() * 100) + 1);
-  var product = { itemName: `${item}`, itemPrice: `${price}`}
+  var price = Math.floor((Math.random() * 100)+1);
+  var product = { itemName: item, itemPrice: price}
   cart.push(product)
 
  return `${item} has been added to your cart.`// write your code here
 }
 
 function viewCart() {
-  var solution = [ "In your cart, you have " ]
+  var tempsolution = []
   if (cart.length === 0) {return "Your shopping cart is empty."}
-
-  for (var i=0; i < cart.length; i++){
-    var price = Math.floor((Math.random() * 100) + 1);
-    solution.push(`${i} at, ${price} `)
-
+  for (var i=0; i < cart.length; i++)
+  if (cart.length === 1) {return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`}
+  else{
+    tempsolution.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
   }
-return solution  // write your code here
+  if (tempsolution.length>1) {
+    var index = tempsolution.length-1
+    tempsolution[index] =  " and" + tempsolution[index]}
+  var solution = "In your cart, you have" + tempsolution.join() + '.'
+return solution
 }
 
 function total() {
@@ -34,7 +37,8 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  delete cart.item
+  if (cart.item === false) {return "That item is not in your cart."}
 }
 
 function placeOrder(cardNumber) {
